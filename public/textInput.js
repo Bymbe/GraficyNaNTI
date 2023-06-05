946c151fe23965fd2686542e17c77695ed146f82
Mateusz Dangreaux <mateuszdangreaux@gmail.com>
2023-06-05 15:54:11 +0200

    sprawdzenie czy użytkownik podał zarówno nazwę jak i treść komentarza

--- public/textInput.js
+++ public/textInput.js
@@ -1,9 +1,18 @@
-document.getElementById("SendButton").onclick = function(){
+document.getElementById("SendButton").onclick = function () {
 
     var author = document.getElementById("authorText").value;
     var comment = document.getElementById("commentText").value;
 
-    document.getElementById("komentarze").innerHTML +=
-        "<br>" + author + " pisze: " + comment;
-    console.log("komentarz ->",comment);
+    if (author) {
+        if (comment) {
+            document.getElementById("komentarze").innerHTML +=
+                "<br>" + author + " pisze: " + comment;
+            console.log("komentarz ->", comment);
+            document.getElementById("errorMsg").innerHTML = "";
+        } else {
+            document.getElementById("errorMsg").innerHTML = "Wpisz treść komentarza!";
+        }
+    } else {
+        document.getElementById("errorMsg").innerHTML = "Podaj swoją nazwę!";
+    }
 }
