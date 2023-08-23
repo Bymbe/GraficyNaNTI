4537712c2906f7e7691e20e6d44554593e36aaa0
RobertK-W <robertk-w@o2.pl>
2023-08-23 18:52:44 +0200

    Dodawanie zdjęć

---
+++ public/loadDevLogs.js
@@ -0,0 +1,35 @@
+document.getElementById("LoadDevLogs").onclick = function (int) {
+    console.log("ładuję plik");
+
+
+    let com = "" ;
+
+    for (let step = 0; step < 100; step++) {
+        if(localStorage.getItem('DEVLOG'+ step)!=null)
+        com = com + "<br><br><br>" +localStorage.getItem('DEVLOG'+ step)
+    }
+
+
+
+    document.getElementById('DEVLOG').innerHTML = com;
+    //let com = localStorage.getItem('Comment') ;
+
+    // var i;
+    // for (i=0;i<10000;i++){
+    //     com = localStorage.getItem('Comment'+i) ;
+    //     document.getElementById('tresc_komentarzy').innerHTML = com;
+    // }
+
+    // var txtFile = new XMLHttpRequest();
+    // var allText = "file not found";
+    // txtFile.onreadystatechange = function () {
+    //     if (txtFile.readyState === XMLHttpRequest.DONE && txtFile.status == 200) {
+    //         allText = txtFile.responseText;
+    //         allText = allText.split("\n").join("<br>");
+    //     }
+    //
+
+    // }
+    // txtFile.open("GET", 'comments.txt', true);
+    // txtFile.send(null);
+}
