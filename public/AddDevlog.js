ba418fb2fff97fa0553872addf20f31f8faf610f
RobertK-W <robertk-w@o2.pl>
2023-08-24 15:33:03 +0200

    W zasadzie to nic nowego

---
+++ public/AddDevlog.js
@@ -0,0 +1,9 @@
+document.getElementById("AddDevLog").onclick = function (int) {
+
+
+    var tresc = document.getElementById('Tresc').value  ;
+    document.getElementById('DL').innerHTML = tresc;
+
+
+
+}
--- public/loadQuestions.js
+++ public/loadQuestions.js
@@ -8,28 +8,6 @@ document.getElementById("LoadQ&A").onclick = function (int) {
         if(localStorage.getItem('Q&A'+ step)!=null)
         com = com + "<br><br><br>" +localStorage.getItem('Q&A'+ step)
     }
-
-
-
     document.getElementById('Q&A').innerHTML = com;
-    //let com = localStorage.getItem('Comment') ;
 
-    // var i;
-    // for (i=0;i<10000;i++){
-    //     com = localStorage.getItem('Comment'+i) ;
-    //     document.getElementById('tresc_komentarzy').innerHTML = com;
-    // }
-
-    // var txtFile = new XMLHttpRequest();
-    // var allText = "file not found";
-    // txtFile.onreadystatechange = function () {
-    //     if (txtFile.readyState === XMLHttpRequest.DONE && txtFile.status == 200) {
-    //         allText = txtFile.responseText;
-    //         allText = allText.split("\n").join("<br>");
-    //     }
-    //
-
-    // }
-    // txtFile.open("GET", 'comments.txt', true);
-    // txtFile.send(null);
 }
