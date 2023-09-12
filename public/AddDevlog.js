fe2095816b4ef73efbe2bbe43bd831efbe497d3f
MichałK <krawczykm@vp.pl>
2023-09-12 12:24:32 +0200

    Deleteing everything

--- public/AddDevlog.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("AddDevLog").onclick = function (int) {
-
-
-    var tresc = document.getElementById('Tresc').value  ;
-    document.getElementById('DL').innerHTML = tresc;
-
-
-
-}
