8aac7ef5f124dc5a73d038a37307001f5fc61bbf
MichałK <krawczykm@vp.pl>
2024-05-30 00:37:33 +0200

    Logowanie i Rejestracja działa jak natura chciała

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import {collection, getDocs, doc, deleteDoc, updateDoc, addDoc} from "firebase/firestore";
+import {collection, getDoc, doc, deleteDoc, updateDoc, addDoc, setDoc} from "firebase/firestore";
 import {db} from "../DataBase/init-firebase"
 
 function Register() {
@@ -9,7 +9,7 @@ function Register() {
     const [Login, setLogin] = useState('');
     const [Password, setPassword] = useState("");
 
-    const createSubcollections = async (mainDocumentRef) => {
+    /*const createSubcollections = async (mainDocumentRef) => {
 
 
         try {
@@ -20,7 +20,7 @@ function Register() {
             console.error(`Błąd przy tworzeniu podkolekcji : `, error); //'${subcollection}'
             throw error;
         }
-    };
+    };*/
 
     const CreateUser = async () => {
         if (Login.trim() === '' || Password.trim() === '' || Name.trim() === '' || Surname.trim() === '') {
@@ -28,8 +28,15 @@ function Register() {
             return;
         }
         try {
-            const mainDocumentRef = await addDoc(collection(db, Login), {placeholderField: true});
-            await createSubcollections(doc(db, Login, mainDocumentRef.id));
+            await setDoc(doc(db, Login,"Dane"),{Imię: Name, Nazwisko: Surname, Hasło: Password} /*{placeholderField: true}*/);
+            const mainDocumentRef = doc(db, Login, "Dane");
+
+            await addDoc(collection(mainDocumentRef, "Zwierzęta"), {placeholderField: true});
+            await addDoc(collection(mainDocumentRef, "Zamówienia"), {placeholderField: true});
+
+            /*await setDoc(doc(db, Login, "Dane", "Zwierzęta", "Burek"), {Rasa: "Golden Retriver"}); // Dodawanie Pupila*/
+
+
             alert(`Kolekcja '${Login}' oraz jej podkolekcje zostały utworzone.`);
         }catch (error) {
             console.error("Błąd przy tworzeniu kolekcji: ", error);
@@ -37,28 +44,63 @@ function Register() {
         }
     };
 
+    const Logowanie = async () => {
+        if (Login.trim() === '' || Password.trim() === '') {
+            alert('Nazwa kolekcji nie może być pusta.');
+            return;
+        }
+        try{
+            const DaneRef = doc(db, Login, "Dane");
+            const dane = await getDoc(DaneRef);
+            if(Password !== dane.data().Hasło){
+                //console.log(dane.data().Hasło);
+                alert('Błędne hasło')
+            }
+            else {
+                alert('Logowanie powiodło się')
+            }
+        }catch (error){
+            console.error("Błąd przy logowaniu ", error);
+            alert('Błąd przy logowaniu');
+        }
+
+    }
+
+
     return (
         <div>
             <div>Register</div>
 
-                <div>
-                    <label htmlFor="Login">Login:</label>
-                    <textarea rows="1" type="text" value={Login} onChange={(e) => setLogin(e.target.value)}/>
-                </div>
-                <div>
-                    <label htmlFor="Password">Password:</label>
-                    <textarea rows="1" type="text" value={Password} onChange={(e) => setPassword(e.target.value)}/>
-                </div>
-                <div>
-                    <label htmlFor="Name">Name:</label>
-                    <textarea rows="1" type="text" value={Name} onChange={(e) => setName(e.target.value)}/>
-                </div>
-                <div>
-                    <label htmlFor="Surname">Surname:</label>
-                    <textarea rows="1" type="text" value={Surname} onChange={(e) => setSurname(e.target.value)}/>
-                </div>
+            <div>
+                <label htmlFor="Login">Login:</label>
+                <textarea rows="1" type="text" value={Login} onChange={(e) => setLogin(e.target.value)}/>
+            </div>
+            <div>
+                <label htmlFor="Password">Password:</label>
+                <textarea rows="1" type="text" value={Password} onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+            <div>
+                <label htmlFor="Name">Name:</label>
+                <textarea rows="1" type="text" value={Name} onChange={(e) => setName(e.target.value)}/>
+            </div>
+            <div>
+                <label htmlFor="Surname">Surname:</label>
+                <textarea rows="1" type="text" value={Surname} onChange={(e) => setSurname(e.target.value)}/>
+            </div>
 
-                <button onClick={CreateUser}>Register</button>
+            <button onClick={CreateUser}>Register</button>
+
+            <div>Logowanie</div>
+            <div>
+                <label htmlFor="Login">Login:</label>
+                <textarea rows="1" type="text" value={Login} onChange={(e) => setLogin(e.target.value)}/>
+            </div>
+            <div>
+                <label htmlFor="Password">Password:</label>
+                <textarea rows="1" type="text" value={Password} onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+            <button onClick={Logowanie}>Login</button>
+            <div>Imię: {Name} Surname: {Surname}</div>
 
 
         </div>
