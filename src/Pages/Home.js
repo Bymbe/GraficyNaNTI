5c187b492e105b3d662bd0a5a451a9a088a44f62
Karolina <101592379+RobertK-W@users.noreply.github.com>
2024-06-04 17:59:07 +0200

    home_bar_2

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Pies from "../Assets/piesek.png";
 import Kot from "../Assets/kotek.png";
+import Karma from "../Assets/karma.png";
 import Logo from "../Assets/Logo-Footer.svg";
 import Twitter from "../Assets/Twitter.svg";
 import {Link} from "react-router-dom";
@@ -33,32 +34,71 @@ function Home() {
                     <h1 id="Karmy-Home">Karmy sezonowe</h1>
                 </div>
                 <br/>
-                <div className="KarmyMainHome">
-                <div className="KarmyBottom-Home">
-                    <button className="KarmyButtonsL-Home">&lt;</button>
-                    <button className="KarmyButtonsR-Home">&gt;</button>
-                    <div className="KarmyBoxy-Home">
-                        <h1 id="NazwaKarmy-Home">"Nazwa Karmy"</h1>
-                        <img src={Pies}/>
-                        <h2>Super dobra karma z płatków kwiatów</h2>
-                        <button className="KarmyButtons2-Home">Czytaj więcej</button>
-                        <button className="KarmyButtons3-Home">Zobacz!</button>
+                <div className="KarmyMain-Home">
+                    <div className="KarmyBottom-Home">
+                        {/*<button className="KarmyButtonsL-Home">&lt;</button>*/}
+                        {/*<button className="KarmyButtonsR-Home">&gt;</button>*/}
+                        {/*<div className="KarmyDekoracyjnieL-Home"></div>*/}
+                        {/*<div className="KarmyDekoracyjnieR-Home"></div>*/}
+                        <div className="KarmyDiv-Home">
+                            <div className="KarmyBoxy-Home">
+                                <h1 id="NazwaKarmy-Home">"Kwiaciara"</h1>
+                                <img src={Karma}/>
+                                <h2>Super dobra karma z płatków kwiatów</h2>
+                                <Link to="/Karmy">
+                                    <button className="KarmyButtons2-Home">Czytaj więcej</button>
+                                </Link>
+                                <button className="KarmyButtons3-Home">Dodaj do koszyka</button>
+                            </div>
+                            <div className="KarmyBoxy-Home">
+                                <h1 id="NazwaKarmy-Home">"Wiosenna energia"</h1>
+                                <img src={Karma}/>
+                                <h2>Warzywny miks na dłuższe dni</h2>
+                                <Link to="/Karmy">
+                                    <button className="KarmyButtons2-Home">Czytaj więcej</button>
+                                </Link>
+                                <button className="KarmyButtons3-Home">Dodaj do koszyka</button>
+                            </div>
+                            <div className="KarmyBoxy-Home">
+                                <h1 id="NazwaKarmy-Home">"Sezonowy owoc"</h1>
+                                <img src={Karma}/>
+                                <h2>Karma z kawałkami sezonowych truskawek</h2>
+                                <Link to="/Karmy">
+                                    <button className="KarmyButtons2-Home">Czytaj więcej</button>
+                                </Link>
+                                <button className="KarmyButtons3-Home">Dodaj do koszyka</button>
+                            </div>
+                        </div>
+                        <div className="KarmyDiv-Home">
+                            <div className="KarmyBoxy-Home">
+                                <h1 id="NazwaKarmy-Home">"Na pyłki"</h1>
+                                <img src={Karma}/>
+                                <h2>Wzmacnia odporność w sezonie alergicznym</h2>
+                                <Link to="/Karmy">
+                                    <button className="KarmyButtons2-Home">Czytaj więcej</button>
+                                </Link>
+                                <button className="KarmyButtons3-Home">Dodaj do koszyka</button>
+                            </div>
+                            <div className="KarmyBoxy-Home">
+                                <h1 id="NazwaKarmy-Home">"W słońcu"</h1>
+                                <img src={Karma}/>
+                                <h2>Na lśniącą sierść</h2>
+                                <Link to="/Karmy">
+                                    <button className="KarmyButtons2-Home">Czytaj więcej</button>
+                                </Link>
+                                <button className="KarmyButtons3-Home">Dodaj do koszyka</button>
+                            </div>
+                            <div className="KarmyBoxy-Home">
+                                <h1 id="NazwaKarmy-Home">"Na ochłodę"</h1>
+                                <img src={Karma}/>
+                                <h2>Karma wspomagająca linienie</h2>
+                                <Link to="/Karmy">
+                                    <button className="KarmyButtons2-Home">Czytaj więcej</button>
+                                </Link>
+                                <button className="KarmyButtons3-Home">Dodaj do koszyka</button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="KarmyBoxy-Home">
-                        <h1 id="NazwaKarmy-Home">"Nazwa Karmy"</h1>
-                        <img src={Pies}/>
-                        <h2>Super dobra karma z płatków kwiatów</h2>
-                        <button className="KarmyButtons2-Home">Czytaj więcej</button>
-                        <button className="KarmyButtons3-Home">Zobacz!</button>
-                    </div>
-                    <div className="KarmyBoxy-Home">
-                        <h1 id="NazwaKarmy-Home">"Nazwa Karmy"</h1>
-                        <img src={Pies}/>
-                        <h2>Super dobra karma z płatków kwiatów</h2>
-                        <button className="KarmyButtons2-Home">Czytaj więcej</button>
-                        <button className="KarmyButtons3-Home">Zobacz!</button>
-                    </div>
-                </div>
 
                 </div>
 
@@ -67,7 +107,6 @@ function Home() {
                 <img src={Kot}/>
 
             </div>
-
 
 
         </div>
