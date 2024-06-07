e97744df919802cbdd4a56e24a8cb23981515a55
RobertK-W <robertk-w@o2.pl>
2024-06-07 13:34:14 +0200

    rejestracja w kwestionariuszu

--- src/Pages/Kwestionariusz.js
+++ src/Pages/Kwestionariusz.js
@@ -1,20 +1,88 @@
 import React, {useState} from 'react'
 import {Link} from "react-router-dom";
 import Karma from "../Assets/karma.png";
+import {addDoc, collection, doc, setDoc} from "firebase/firestore";
+import {db} from "../DataBase/init-firebase";
 
 function Kwestionariusz(props) {
 
     const [Dalej, setDalej] = useState(false);
     const [DodajDoKoszyka, setDodajDoKoszyka] = useState(false);
+    const [Rejestracja, setRejestracja] = useState(false);
 
     const dalejFunction = async ()=> {
         setDalej(true);
+        setRejestracja(false);
         return;
     }
     const DodajDoKoszykaFunction = async ()=> {
         setDodajDoKoszyka(true);
+        setRejestracja(false);
         return;
     }
+
+    const RejestracjaFunction = async ()=> {
+        setRejestracja(true);
+        setDalej(false);
+        setDodajDoKoszyka(false);
+        return;
+    }
+
+    const [Name, setName] = useState("");
+    const [Surname, setSurname] = useState("");
+    const [Login, setLogin] = useState('');
+    const [Email, setEmail] = useState('');
+    const [Telephone, setTelephone] = useState("");
+    const [Password, setPassword] = useState("");
+    const [PasswordRepeated, setPasswordRepeated] = useState("");
+    const [Zalogowano, setZalogowano] = useState(false);
+    const [Regulamin, setRegulamin] = useState(false);
+
+
+    const CreateUser = async () => {
+
+        if (Regulamin === false) {
+            alert('Zaznacz wszystkie wymagane pola')
+            return;
+        }
+
+        if (Login.trim() === '' || Password.trim() === '' || Name.trim() === '' || Surname.trim() === ''|| Surname.trim() === '') {
+            alert('Nazwa kolekcji nie może być pusta.');
+            return;
+        }
+
+
+
+
+        if (Password !== PasswordRepeated) {
+            alert('Hasła nie są takie same')
+            return;
+        }
+        try {
+            await setDoc(doc(db, Login,"Dane"),{Imię: Name, Nazwisko: Surname, Hasło: Password, E_Mail: Email, Telefon: Telephone} /*{placeholderField: true}*/);
+            const mainDocumentRef = doc(db, Login, "Dane");
+
+            await addDoc(collection(mainDocumentRef, "Zwierzęta"), {placeholderField: true});
+            await addDoc(collection(mainDocumentRef, "Zamówienia"), {placeholderField: true});
+            await addDoc(collection(mainDocumentRef, "Koszyk"), {placeholderField: true});
+
+            /*await setDoc(doc(db, Login, "Dane", "Zwierzęta", "Burek"), {Rasa: "Golden Retriver"}); // Dodawanie Pupila*/
+            setZalogowano(true);
+            props.handleCallBackZalogowo(Zalogowano);
+            props.handleCallBackLogin(Login);
+            alert(`Kolekcja '${Login}' oraz jej podkolekcje zostały utworzone.`);
+        }catch (error) {
+            console.error("Błąd przy tworzeniu kolekcji: ", error);
+            alert('Błąd przy tworzeniu kolekcji');
+        }
+    };
+
+
+
+
+
+
+
 
     return (
         <div className="Kwestionariusz">
@@ -157,22 +225,98 @@ function Kwestionariusz(props) {
                 </div>
 
             </div>
+
             <div className="DDK">
                 {DodajDoKoszyka ? (
                     <div className="DodajDoKoszyka-Popup">
                         <h1>Czy chcesz założyć konto?</h1>
                         {/*<h2>Możesz teraz przejść do swojego profilu</h2>*/}
-                        <Link to="/Register">
-                            <button>Załóż konto</button>
-                        </Link>
+
+                        <button onClick={RejestracjaFunction}>Załóż konto</button>
+
                         <Link to="/Koszyk">
                             <button>Kupuję jednorazowo</button>
                         </Link>
                         <h2></h2>
                     </div>
                 ) : (<div style={{display: "none"}}></div>)}
-            </div>
 
+            </div>
+            {Rejestracja ? (
+            <div className="Rejestr">
+
+
+                    <div className="RejestrPopup">
+
+                        <div className="Register1">
+                            <h1>Register</h1>
+                            <h3></h3>
+
+                            <div className="Register-Text-AreaKW">
+                                <label htmlFor="Login">Login:</label>
+                                <textarea rows="1" type="text" value={Login}
+                                          onChange={(e) => setLogin(e.target.value)}/>
+                            </div>
+                            <div className="Register-Text-AreaKW">
+                                <label htmlFor="Password">Password:</label>
+                                <textarea rows="1" type="text" value={Password}
+                                          onChange={(e) => setPassword(e.target.value)}/>
+                            </div>
+                            <div className="Register-Text-AreaKW">
+                                <label htmlFor="Password">Repeat Password:</label>
+                                <textarea rows="1" type="text" value={PasswordRepeated}
+                                          onChange={(e) => setPasswordRepeated(e.target.value)}/>
+                            </div>
+                            <div className="Register-Text-AreaKW">
+                                <label htmlFor="EMail">E-mail:</label>
+                                <textarea rows="1" type="text" value={Email}
+                                          onChange={(e) => setEmail(e.target.value)}/>
+                            </div>
+                            <div className="Register-Text-AreaKW">
+                                <label htmlFor="Imię">Imię:</label>
+                                <textarea rows="1" type="text" value={Name} onChange={(e) => setName(e.target.value)}/>
+                            </div>
+                            <div className="Register-Text-AreaKW">
+                                <label htmlFor="Nazwisko">Nazwisko:</label>
+                                <textarea rows="1" type="text" value={Surname}
+                                          onChange={(e) => setSurname(e.target.value)}/>
+                            </div>
+                            <div className="Register-Text-AreaKW">
+                                <label htmlFor="Tel">Telefon:</label>
+                                <textarea rows="1" type="text" value={Telephone}
+                                          onChange={(e) => setTelephone(e.target.value)}/>
+                            </div>
+                            <div className="Register-CheckBox-AreaKW">
+                                <input type="checkbox" onChange={(e) => setRegulamin(!Regulamin)}/>
+                                <h3 data-end="*">Akceptuję warunki regulaminu</h3>
+                            </div>
+
+
+                            <button onClick={CreateUser}>Register</button>
+
+                            {Zalogowano ? (
+                                <div className="Register-Popup">
+                                    <h1>Zakładanie konta zakończyło się sukcesem!</h1>
+                                    <h2>Możesz teraz przejść do swojego profilu</h2>
+                                    <Link to="/Konto">
+                                        <button>Przejdź do Profilu</button>
+                                    </Link>
+                                </div>
+                            ) : (<div style={{display: "none"}}></div>)}
+                        </div>
+                        {/*<h1>Czy chcesz założyć konto?</h1>*/}
+                        {/*/!*<h2>Możesz teraz przejść do swojego profilu</h2>*!/*/}
+
+                        {/*<button onClick={RejestracjaFunction}>Załóż konto</button>*/}
+
+                        {/*<Link to="/Koszyk">*/}
+                        {/*    <button>Kupuję jednorazowo</button>*/}
+                        {/*</Link>*/}
+                        {/*<h2></h2>*/}
+                    </div>
+
+            </div>
+            ) : (<div style={{display: "none"}}></div>)}
         </div>
     )
 }
